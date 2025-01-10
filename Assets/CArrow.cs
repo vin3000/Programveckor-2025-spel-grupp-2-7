89b49794b80fe550a374c429d553d7104028ae75
Ivanholfve <153901435+Ivanholfve@users.noreply.github.com>
2025-01-10 12:45:57 +0100

    you can move now

--- Assets/CArrow.cs
+++ Assets/CArrow.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Arrow : MonoBehaviour
+public class CArrow : MonoBehaviour
 {
     float life;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
