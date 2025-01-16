c9a1ecfde507988f55f2adc5eff4678acb86d2ac
Ivanholfve <153901435+Ivanholfve@users.noreply.github.com>
2025-01-16 17:46:18 +0100

    death screen works
    I HATE CROUCHING!!!

--- Assets/CristianScripts/PlayerHealth.cs
+++ Assets/CristianScripts/PlayerHealth.cs
@@ -35,7 +35,7 @@ public class PlayerHealth : MonoBehaviour
             nrOfPotions -= 1;
 
         }
-        if (health <= 0)
+        if (health <= 0 && gameOverScript.gameHasEnded == false)
         {
             gameOverScript.Setup();
         }
--- Assets/IvanScripts/CameraMove.cs
+++ Assets/IvanScripts/CameraMove.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class CameraMove : MonoBehaviour
 {
 
     public float mouseSensitivity = 100f;
