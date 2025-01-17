935cb9f3318d47e7fd0b900747af39cd9b857438
GrandpaMiles <milton.hernvald@elev.ga.lbs.se>
2025-01-17 11:02:31 +0100

    npc jerry

--- Assets/Cutscene.cs
+++ Assets/Cutscene.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Cutscene : MonoBehaviour
 {
@@ -9,24 +10,31 @@ public class Cutscene : MonoBehaviour
     public Camera cutsceneCamera;
     public TextMeshProUGUI text;
     AudioSource voice;
+    Rigidbody rb;
+
+    float cutsceneTime=0;
     void Start()
     {
         cutsceneAnimation = GetComponent<Animation>();
         voice = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (cutsceneAnimation.isPlaying == false && voice.isPlaying == false)
+        cutsceneTime += Time.deltaTime;
+        if (cutsceneAnimation.isPlaying == false && cutsceneTime > 11f)
         {
             Player.SetActive(true);
             text.enabled = false;
+            rb.isKinematic = false;
 
         }
         else
         {
             cutsceneCamera.gameObject.SetActive(true);
+            rb.isKinematic = true;
             
         }
         
