4000d53745290daa39d9873f6137538f9e424797
vin3000 <vincent.romer@elev.ga.lbs.se>
2025-01-27 15:20:55 +0100

    Jerry Dialogue box
    texten skrollar nu :D

--- Assets/Scripts/Cutscene.cs
+++ Assets/Scripts/Cutscene.cs
@@ -1,42 +1,41 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class Cutscene : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public GameObject Player;
+    [SerializeField] private GameObject Player;
     Animation cutsceneAnimation;
-    public Camera cutsceneCamera;
-    public TextMeshProUGUI text;
+    [SerializeField] private Camera cutsceneCamera;
+    [SerializeField] private GameObject cutsceneTextBox;
+    [SerializeField] private TextMeshProUGUI cutsceneText;
+    private string tempText;
+    private float scrollSpeed = 0.05f;
+    private bool isScrolling = false;
     AudioSource voice;
     Rigidbody rb;
 
-    float cutsceneTime=0;
     void Start()
     {
+        Player.SetActive(false);
         cutsceneAnimation = GetComponent<Animation>();
         voice = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
+        tempText = cutsceneText.text;
+        cutsceneText.text = "";
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        cutsceneTime += Time.deltaTime;
-        if (cutsceneAnimation.isPlaying == false && cutsceneTime > 11f)
+        if(Input.GetKeyDown(KeyCode.Q))
         {
-            Player.SetActive(true);
-            text.enabled = false;
-            rb.isKinematic = false;
-            cutsceneCamera.gameObject.SetActive(false);
-
-        }
-        else
-        {
-            cutsceneCamera.gameObject.SetActive(true);
-            rb.isKinematic = true;
-            
+            EnablePlayer();
+            TextDisable();
         }
         
         
@@ -44,12 +43,39 @@ public class Cutscene : MonoBehaviour
     }
     public void TextEnable()
     {
-        text.enabled = true;
+
+        cutsceneTextBox.SetActive(true);
+        StartCoroutine(scrollText());
         voice.Play();
     }
+
+    IEnumerator scrollText()
+    {
+        if (isScrolling) StopCoroutine(scrollText());
+        isScrolling = true;
+        for (int i = 0; i < tempText.Length; i++)
+        {
+            cutsceneText.text += tempText.Substring(i, 1);
+            yield return new WaitForSeconds(scrollSpeed);
+            isScrolling = false;
+        }
+        EnablePlayer();
+        yield return new WaitForSeconds(5f);
+        TextDisable();
+
+    }
+
+    private void EnablePlayer()
+    {
+        Player.SetActive(true);
+        rb.isKinematic = false;
+        cutsceneCamera.gameObject.SetActive(false);
+    }
+
     public void TextDisable()
     {
-        
+        cutsceneTextBox.SetActive(false);
+        voice.Stop();
     }
 
 }
