e85b3baa69e5afe42f02aa27f565673f03ae10fd
Ivanholfve <153901435+Ivanholfve@users.noreply.github.com>
2025-01-31 13:27:35 +0100

    fixed wall in boss room

--- Assets/CristianScripts/Dissapear.cs
+++ Assets/CristianScripts/Dissapear.cs
@@ -1,25 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class Dissapear : MonoBehaviour
 {
-    float dissapearTimer=10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(destroyAfterTime());
     }
 
     // Update is called once per frame
+    IEnumerator destroyAfterTime() 
+    {
+        yield return new WaitForSeconds(30);
+        Destroy(gameObject);
+    }
     void Update()
     {
-        dissapearTimer -= Time.deltaTime;
-        if (dissapearTimer <= 0)
-        {
-            Destroy(gameObject);
-        }
+        
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        print(collision.gameObject.name);
-    }
+    
 }
